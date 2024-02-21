04f01a5dd57dd62d851957d99b42e83e701ba25c
lingeshnafeax <wlingesh260@gmail.com>
2024-02-21 10:35:35 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import { Content } from "./components/Content";
 import { SkillsContainer } from "./components/SkillsContainer";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ function Header() {
   return (
     <div className="pt-4 flex w-auto gap-x-6 items-center font-serif border pb-4 border-b-2 bg-slate-200">
       <img className=" h-14 w-14 rounded-full ml-8" src={logo}></img>
-      <span className=" whitespace-nowrap">About Me</span>
-      <span>Projects</span>
+      <div className="flex gap-x-4 w-full justify-end pr-5">
+        <span className=" whitespace-nowrap">About Me</span>
+        <span>Projects</span>
+      </div>
     </div>
   );
 }
